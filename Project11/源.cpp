3ffabb8d49ff2d0835f0a7bcb649c422cdb07632
yuekeru <38829484@qq.com>
2025-10-20 17:42:53 +0800

    添加判断棋盘是否已满的函数并删除了settarget(nullptr)

--- Project11/源.cpp
+++ Project11/源.cpp
@@ -396,6 +396,7 @@ public:
 	}
 	//输出获胜者信息
 	
+	
 	void putWinner(const char* winnerInfo)
 	{
 		int width = 400;
@@ -410,10 +411,26 @@ public:
 		setcolor(BLACK);
 		rectprintf(x, y, w, h,winnerInfo);
 		
-		settarget(nullptr);
+		
 		int winX = (WINDOW_WIDTH - width) / 2;
 		int winY = (WINDOW_HEIGHT - height) / 2;
 		putimage_withalpha(NULL, m_endImage, winX, winY);
+
+		return;
+	}
+	//判断棋盘是否已满
+	void ifboardFull()
+	{
+		for (int i = 0; i < BOARD_SIZE; i++)
+		{
+			for (int j = 0; j < BOARD_SIZE; j++)
+			{
+				if (m_board[i][j] == EMPTY)
+				{
+
+				}
+			}
+		}
 		return;
 	}
 	
