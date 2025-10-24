f5f3cbc5206acc366092bde565054ab623ecad93
yuekeru <38829484@qq.com>
2025-10-24 19:44:56 +0800

    ’这次可否提交并同步呢？‘

--- Project11/源.cpp
+++ Project11/源.cpp
@@ -420,9 +420,11 @@ public:
 	}
 	
 	
+	
 	GameState getGameState() const { return gameState; }
 	PieceType getcurrentPlayer() const { return m_currentPlayer; }
-	//我的更改去了哪里？
+
+	
 
 private:
 	int m_lastRow{};
